2378228e63ee597a4e7b18df8b98d71e5b6202aa
pashagorbovsky <pashagorbovsky@gmail.com>
2024-11-16 12:51:50 +0300

    Add files via upload

---
+++ toro.py
@@ -0,0 +1,17 @@
+#1st program
+print('task1')
+print(9 ** 0.5 * 5)
+#2st program
+print('task2')
+print(9.99>9.98 and 1000!=1000.1)
+#3st program
+print('task3')
+print(2 * 2 + 2)
+print(2 * (2+2))
+print((2 * 2 + 2) == (2 * (2+2)))
+#4st program
+print('task4')
+print((int((float('123.456')) * 10)) % 10)
+
+
+
