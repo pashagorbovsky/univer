c363cab644d332e88de5f88dd6fc61823ea0f9d5
pashagorbovsky <pashagorbovsky@gmail.com>
2024-11-25 23:54:58 +0300

    Update module_1.py

--- module_1.py
+++ module_1.py
@@ -5,11 +5,11 @@ print(type(end))
 students = list(students)
 students = sorted(students)
 
-grades2 = [sum(grades[0]) / len(grades[0]),
+grades = [sum(grades[0]) / len(grades[0]),
            sum(grades[1]) / len(grades[1]),
            sum(grades[2]) / len(grades[2]),
            sum(grades[3]) / len(grades[3]),
            sum(grades[4]) / len(grades[4])]
-end.update({students[0]: grades2[0],students[1]: grades2[1],students[2]: grades2[2],students[3]: grades2[3],students[4]: grades2[4] })
+end.update({students[0]: grades[0],students[1]: grades[1],students[2]: grades[2],students[3]: grades[3],students[4]: grades[4] })
 
 print(end)
