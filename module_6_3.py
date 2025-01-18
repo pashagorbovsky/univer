8949c8c7d3475de53c51d6d7e5d1cc54664b704f
pashagorbovsky <pashagorbovsky@gmail.com>
2025-01-18 16:20:45 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,66 @@
+from random import randrange
+
+class Animal:
+    live = True
+    _DEGREE_OF_DANGER = 0
+    def __init__(self, speed):
+        self._cords = [0, 0, 0]
+        self.speed = speed
+        super().__init__()
+
+
+
+    def move(self, dx, dy, dz):
+        for i in [dx, dy, dz]:
+            if i < 0:
+                print("It's too deep, i can't dive")
+                break
+            else:
+                self._cords[0] = dx * self.speed
+                self._cords[1] = dy * self.speed
+                self._cords[2] = dz * self.speed
+
+    def get_cords(self):
+        print(f'X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, i'm peaceful")
+        else:
+            print("Be careful, I'm attacking you 0_0")
+
+    def speak(self):
+        print(self.sound)
+
+class Bird(Animal):
+    beak = True
+    def lay_eggs(self):
+        print(f'Here are(is) {randrange(1,4)} egs for you')
+
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+    def dive_in(self, dz):
+        speed1 = self.speed / 2
+        self._cords[2] -= dz * speed1
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+class Duckbill(PoisonousAnimal, Bird, AquaticAnimal):
+    sound = 'click-click-click'
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1,2,3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
